75bf8256246f6a423fe4e1b2df9fd7e43acf04f1
hungsinh2k4 <monternsh@gmail.com>
2023-11-30 22:21:47 +0700

    update signup no mongodb

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-// var Bear = require('./app/models/bear');
-app.listen(8000);
+
+//app.listen(8000);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.get('/register', (req, res) => {
-    res.send('Hello World!');
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    // Access form data using req.body
+    const { username, email, password } = req.body;
+
+    // Process the form data (you might want to store it in a database, for example)
+
+    // Respond to the client
+    res.send(`Registration successful!\nUsername: ${username}\nEmail: ${email}\nPassword: ${password}`);
+});
+
+app.listen(8000, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+    } else {
+        console.log('Server is listening on port 8000');
+    }
 });
