864c503699e3477969bb2b8640e45cb90b13b109
hungsinh2k4 <monternsh@gmail.com>
2023-12-01 15:55:30 +0700

    update app.js

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-//app.listen(8000);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+// cau hinh ejs
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
+// nhan du lieu tu form
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var user = require('./models/user');
+const { default: mongoose } = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/myapp');
+
 
 app.get('/signup', (req, res) => {
     res.render('signup');
@@ -26,17 +32,21 @@ app.post('/signin', (req, res) => {
 
     // Respond to the client
     res.send(`Registration successful!\nUsername: ${username}\nEmail: ${email}\nPassword: ${password}`);
-    res.redirect('/signin');
 });
 
-app.post('/signup', (req, res) => {
-    const { username, email, password } = req.body;
-    // Process the form data (you might want to store it in a database, for example)
-    // Respond to the client
-    res.redirect('/signin');
+app.post('/signup', async (req, res) => {
+    // Access form data using req.body
+    try {
+        const newUser = await user.create({ username: req.body.username, email: req.body.email, password: req.body.password });
+        // Redirect to the login page
+        res.redirect('/signin');
+    } catch (err) {
+        console.error(err);
+        res.send('Error creating user');
+    }
 });
 
-app.listen(8000, (err) => {
+app.listen(8001, (err) => {
     if (err) {
         console.error('Error starting server:', err);
     } else {
